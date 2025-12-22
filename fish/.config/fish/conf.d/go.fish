617f8268f88d50293cb9a8f8776230a9556f1d03
Stefan Bildl <stefanbildl@me.com>
2025-12-22 10:23:54 +0100

    use cachyOS fish config basis but remove /usr/ references

---
+++ fish/.config/fish/conf.d/go.fish
@@ -0,0 +1,13 @@
+
+if status is-interactive
+    set -l go_bin
+    if set -q GOBIN
+        set go_bin $GOBIN
+    else
+        set go_bin (go env GOPATH)/bin
+    end
+
+    if test -d $go_bin
+        contains -- $go_bin $fish_user_paths || set -Ua fish_user_paths $go_bin
+    end
+end
