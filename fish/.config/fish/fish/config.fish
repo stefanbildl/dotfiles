28053585ee2c6f8832b55f58347fb2bc7ca521dc
Stefan Bildl <stefanbildl@me.com>
2025-12-22 09:07:24 +0100

    use fish

---
+++ fish/.config/fish/fish/config.fish
@@ -0,0 +1,8 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+starship init fish | source
+# overwrite greeting
+# potentially disabling fastfetch
+function fish_greeting;end
+
+fzf --fish | source
+
