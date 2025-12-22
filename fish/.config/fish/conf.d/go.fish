1405dea628de1838067a1ce733065678c8f90563
Stefan Bildl <stefanbildl@me.com>
2025-12-22 11:42:56 +0100

    fixup

--- fish/.config/fish/conf.d/go.fish
+++ fish/.config/fish/conf.d/go.fish
@@ -1,13 +1,14 @@
-
 if status is-interactive
-    set -l go_bin
-    if set -q GOBIN
-        set go_bin $GOBIN
-    else
-        set go_bin (go env GOPATH)/bin
-    end
+   if command -q go
+        set -l go_bin
+        if set -q GOBIN
+            set go_bin $GOBIN
+        else
+            set go_bin (go env GOPATH)/bin
+        end
 
-    if test -d $go_bin
-        contains -- $go_bin $fish_user_paths || set -Ua fish_user_paths $go_bin
+        if test -d $go_bin
+            contains -- $go_bin $fish_user_paths || set -Ua fish_user_paths $go_bin
+        end
     end
 end
--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 # Colors for output
 RED='\033[0;31m'
 GREEN='\033[0;32m'
